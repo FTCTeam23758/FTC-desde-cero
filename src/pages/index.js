afcec6b2d4d35027ed9a20e0c4475984601c971d
alfonsodeanda <ponchodeandan@gmail.com>
2025-10-26 00:49:11 -0600

    added information and fixed tabs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,3 @@
-/*const React = require('react');
-const Redirect = require('@docusaurus/router').Redirect;
-
-function Home() {
-  return React.createElement(Redirect, { to: '/intro' });
-}
-
-module.exports = Home;*/
-
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import React from 'react';
 import { Redirect } from '@docusaurus/router';
